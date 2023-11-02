fa1dee8ce306b39fe37c714d5a8168fbd0dcd6e5
pdvarela <peter.varela08gmail.com>
2023-11-01 21:50:59 -0500

    Ultimos ajustes preentrega

--- desafio_3/src/app.js
+++ desafio_3/src/app.js
@@ -6,11 +6,11 @@ const jsonFile = "./productsFile.json";
 
 //La siguiente validacion permite agregar los productos y generar el archivo de productos para las respectivas pruebas sin dejar un archivo ya creado en el sistema de archivos
 if (!fs.existsSync(jsonFile)) {
-    productManager.addProduct("botas","Botas negras de cuero",29.30,"urldelaimagen","pp3499",56);      //Agrega un Producto Valido ID 1
-    productManager.addProduct("camisa","camisa de lana clase A",29.30,"urldelaimagen1","39995asd",56);   // Agrega un Producto Valido ID 2
-    productManager.addProduct("camisa","camisa de lana clase B",10.75,"urldelaimagen2","2435asd",56);   // Agrega un Producto Valido ID 3
+    productManager.addProduct("botas","Botas negras de cuero",29.30,"urldelaimagen","pp3499",56);
+    productManager.addProduct("camisa","camisa de lana clase A",29.30,"urldelaimagen1","39995asd",56);   
+    productManager.addProduct("camisa","camisa de lana clase B",10.75,"urldelaimagen2","2435asd",56);   
     productManager.addProduct("sueter","Sueter de lana clase B",25,"urldelaimagen3","rrr35as",5);       
-    productManager.addProduct("casco","casco de ciclismo Mips",33.50,"urldelaimagen4","2445asd",80);    // Genera error Codigo repetido ⛔
+    productManager.addProduct("casco","casco de ciclismo Mips",33.50,"urldelaimagen4","2445asd",80);    
     productManager.addProduct("zapato ciclismo","Zapato de ciclismo ZEBRA",90.50,"urldelaimagen5","2935asd",80);
     productManager.addProduct("Mochila MTB","Mochila de hidratacion para MTB",30,"urldelaimagen6","3435kst",40);
     productManager.addProduct("Bicicleta Scott Scale 920","Scott Scale 920 Talla M",2500,"urldelaimagen7","5435asu",12);
